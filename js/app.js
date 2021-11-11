7dd874a285bbf2b5b024b3ba929a9682d755a7e0
dpetrice <66479818+dpetrice@users.noreply.github.com>
2021-11-11 16:06:38 +0100

    test

--- js/app.js
+++ js/app.js
@@ -43,25 +43,6 @@ function getUserMedia(options, successCallback, failureCallback) {
 var theStream;
 var theRecorder;
 var recordedChunks = [];
-var target = document.getElementById('target');
-var watchId;
-
-function appendLocation(location, verb) {
-  verb = verb || 'updated';
-  var newLocation = document.createElement('p');
-  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
-  target.appendChild(newLocation);
-}
-
-if ('geolocation' in navigator) {
-  document.getElementById('askButton').addEventListener('click', function () {
-    navigator.geolocation.getCurrentPosition(function (location) {
-      appendLocation(location, 'fetched');
-    });
-    watchId = navigator.geolocation.watchPosition(appendLocation);
-  });
-} else {
-  target.innerText = 'Geolocation API not supported.';
 
 function getStream() {
   if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
@@ -124,4 +105,4 @@ function getBase64Image(img) {
 
 var base64 = getBase64Image(document.getElementById("imageTag"));
 }
-}
+
