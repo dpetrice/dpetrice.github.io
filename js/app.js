2e94f514ffac93d728b50c4278e601c04e05732b
dpetrice <66479818+dpetrice@users.noreply.github.com>
2021-11-25 16:18:14 +0100

    update

--- js/app.js
+++ js/app.js
@@ -25,6 +25,51 @@ if ("serviceWorker" in navigator) {
         document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
       });
   }
+
+  var $status = document.getElementById('status');
+
+if ('Notification' in window) {
+  $status.innerText = Notification.permission;
+}
+
+function requestPermission() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  Notification.requestPermission(function (result) {
+    $status.innerText = result;
+  });
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function nonPersistentNotification() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  try {
+    var notification = new Notification("Hi there - non-persistent!");
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+}
+
+function persistentNotification() {
+  if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+    alert('Persistent Notification API not supported!');
+    return;
+  }
+  
+  try {
+    navigator.serviceWorker.getRegistration()
+      .then((reg) => reg.showNotification("Hi there - persistent!"))
+      .catch((err) => alert('Service Worker registration error: ' + err));
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+}
   
   function requestPersistence() {
     if ('storage' in navigator && 'persist' in navigator.storage) {
@@ -34,7 +79,7 @@ if ("serviceWorker" in navigator) {
         });
     }
   }
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   function getReadFile(reader, i) {
     return function () {
       var li = document.querySelector('[data-idx="' + i + '"]');
