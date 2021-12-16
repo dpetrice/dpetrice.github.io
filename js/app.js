97c5fef83729bed7ad0195b0f79ec415468e7a0f
dpetrice <66479818+dpetrice@users.noreply.github.com>
2021-12-16 15:05:17 +0100

    s

--- js/app.js
+++ js/app.js
@@ -21,11 +21,7 @@ if ("serviceWorker" in navigator) {
     var tiltLR = eventData.gamma;
     var tiltFB = eventData.beta;
     var dir = eventData.alpha;
-    /*
-    document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
-    document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
-    document.getElementById("doDirection").innerHTML = Math.round(dir);
-  */
+
     var logo = document.getElementById("imgLogo");
     logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
     logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
