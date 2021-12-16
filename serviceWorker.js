d2c1730626c6f52c61a7755e1e29958026838f57
dpetrice <66479818+dpetrice@users.noreply.github.com>
2021-12-16 16:06:31 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,10 @@
 const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/tabelle.png"
+  //"/",
+  //"/index.html",
+  //"/css/style.css",
+  //"/js/app.js",
+ // "/tabelle.png"
 ];
 
 self.addEventListener("install", installEvent => {
@@ -28,7 +28,7 @@ self.addEventListener("fetch", fetchEvent => {
 const expectedCaches = ["dev-coffee-site-v1"];
 
 self.addEventListener('install', event => {
-console.log('V2 installing…'+"Service worker version="+serviceworker_version);
+console.log('V3 installing…');
 caches.delete("dev-coffee-site-v1");
 console.log("Old cache deleted");
 // cache a horse SVG into a new cache, static-v2
