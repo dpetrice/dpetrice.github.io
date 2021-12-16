8ee2c85c91f415f878eac4313a7d11b9854f4ee1
dpetrice <66479818+dpetrice@users.noreply.github.com>
2021-12-16 15:14:38 +0100

    h

--- js/app.js
+++ js/app.js
@@ -17,17 +17,7 @@ if ("serviceWorker" in navigator) {
     document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
   }
   
-  function deviceOrientationHandler (eventData) {
-    var tiltLR = eventData.gamma;
-    var tiltFB = eventData.beta;
-    var dir = eventData.alpha;
 
-    var logo = document.getElementById("imgLogo");
-    logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
-    logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
-    logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)"; }
-     /* Device Position
-  */
  
   function getUserMedia(options, successCallback, failureCallback) {
     var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
