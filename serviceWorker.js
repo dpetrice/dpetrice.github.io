e0e61f720c1d803d73199ee208eb3936337ded18
dpetrice <66479818+dpetrice@users.noreply.github.com>
2021-12-16 14:56:22 +0100

    j

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,44 +4,61 @@ const assets = [
   "/index.html",
   "/css/style.css",
   "/js/app.js",
+  "/tabelle.png"
 ];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
+      caches.open(staticDevCoffee).then(cache => {
+          cache.addAll(assets)
+      })
+  )
+})
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
+      caches.match(fetchEvent.request).then(res => {
+          return res || fetch(fetchEvent.request)
+      })
+  )
+})
+
+
+
+
+
+const expectedCaches = ["dev-coffee-site-v1"];
+
+self.addEventListener('install', event => {
+console.log('V1 installing…'+"Service worker version="+serviceworker_version);
+caches.delete("dev-coffee-site-v1");
+console.log("old cache deleted");
+// cache a horse SVG into a new cache, static-v2
+caches.open(staticDevCoffee).then(cache => {
+  cache.addAll(assets)
+}) 
 });
 
+
 self.addEventListener('activate', event => {
-  console.log("Service worker activated, deleting cache now");
-  // delete any caches that aren't in expectedCaches
-  // which will get rid of static-v1
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (!expectedCaches.includes(key)) {
-          return caches.delete(key);
-        }
-      })
-    )).then(() => {
-      console.log('V2 now ready to handle fetches!');
+console.log("Service worker activated, deleting cache now");
+// delete any caches that aren't in expectedCaches
+// which will get rid of static-v1
+event.waitUntil(
+  caches.keys().then(keys => Promise.all(
+    keys.map(key => {
+      if (!expectedCaches.includes(key)) {
+        return caches.delete(key);
+      }
     })
-  );
+  )).then(() => {
+    console.log('V2 now ready to handle fetches!');
+  })
+);
 });
 
 
 
 self.addEventListener('message', function(event) {
-  event.ports[0].postMessage({'test': 'This is my response.'});
+event.ports[0].postMessage({'test': 'This is my response.'});
 });
-
